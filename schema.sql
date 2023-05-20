e20481934b6acd5f3967722bb264e1056a647ca5
saifalkurdi <saifalkurdi66@gmail.com>
2023-05-20 17:41:24 +0000

    make changes to create netflix app.

--- schema.sql
+++ schema.sql
@@ -3,7 +3,8 @@ create table if not exists movie_data(
   title varchar(255),
   release_date varchar(255),
   poster_path varchar(1000),
-  overview varchar (1500)
+  overview varchar (1500),
+  comments varchar(255)
 ); 
 
-insert into movie_data (title, release_date, poster_path, overview) values('test',45,'anything','http://google.com')
+insert into movie_data (title, release_date, poster_path, overview, comments) values('test',45,'anything','http://google.com', 'test comments')
--- server.js
+++ server.js
@@ -13,27 +13,42 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
-app.get('/', (req , res )=> {
-  const movie = new Movie(data.id, data.title, data.release_date, data.poster_path, data.overview,)
-  res.send(movie)
-})
-app.get('/trending', handleRec);
-app.get('/getMovies', getMovie);
-app.post('/getMovies', addMovie);
-app.get('/getMovies/:id', getMovieById);
+const PORT = process.env.PORT || 4000;
+app.get("/", (req, res) => {
+  const movie = new Movie(
+    data.id,
+    data.title,
+    data.release_date,
+    data.poster_path,
+    data.overview
+  );
+  res.send(movie);
+});
+app.get("/trending", handleRec);
+app.get("/getMovies", getMovie);
+app.post("/addMovie", addMovie);
+app.get("/getMovies/:id", getMovieById);
 // http://localhost:PORT/link/id
-app.put('/getMovies/:id', updateMovieById);
-app.delete('/getMovies/:id',deleteMovieById);
+app.put("/getMovies/:id", updateMovieById);
+app.delete("/getMovies/:id", deleteMovieById);
 
 app.use(errorHandler);
 
 async function handleRec(req, res) {
   const axiosCallApi = await axios.get(
-    `${process.env.URL}?api_key=${process.env.APIKEY}`);
-    Movie.allData = [];
-  axiosCallApi.data.results.map((item) =>
-  new Movie(item.id, item.title,item.release_date,item.poster_path,item.overview )
+    `${process.env.URL}?api_key=${process.env.APIKEY}`
+  );
+  Movie.allData = [];
+  console.log(axiosCallApi.data.results);
+  axiosCallApi.data.results.map(
+    (item) =>
+      new Movie(
+        item.id,
+        item.title,
+        item.release_date,
+        item.poster_path,
+        item.overview
+      )
   );
   res.status(200).json({
     code: 200,
@@ -41,59 +56,72 @@ async function handleRec(req, res) {
   });
 }
 
-
 function getMovie(req, res) {
   const sql = `select * from movie_data`;
-  client.query(sql).then((data) => {
+  client
+    .query(sql)
+    .then((data) => {
       res.json(data.rows);
-    }).catch((err) => {
+    })
+    .catch((err) => {
       errorHandler(err, req, res);
     });
 }
 
 function addMovie(req, res) {
   const userInput = req.body;
-  const sql = `insert into movie_data (title, release_date, poster_path, overview) values ($1, $2, $3, $4) returning *`;
+  const sql = `insert into movie_data (title, release_date, poster_path, overview, comments) values ($1, $2, $3, $4, $5) returning *`;
 
   const handleValueForUser = [
     userInput.title,
     userInput.release_date,
     userInput.poster_path,
     userInput.overview,
+    userInput.comments,
   ];
-  client.query(sql, handleValueForUser).then((data) => {
+  client
+    .query(sql, handleValueForUser)
+    .then((data) => {
       res.status(201).json(data.rows[0]);
-    }).catch((err) => errorHandler(err, req, res));
+    })
+    .catch((err) => errorHandler(err, req, res));
 }
 
-function getMovieById(req, res){
+function getMovieById(req, res) {
   Movie.allData = [];
   const id = req.params.id;
   const sql = `select * from movie_data where id = ${id}`;
-  client.query(sql).then( data => 
-    res.status(200).json(data.rows)
-  )
+  client.query(sql).then((data) => res.status(200).json(data.rows));
 }
 
-function updateMovieById (req, res) {
+function updateMovieById(req, res) {
   const id = req.params.id;
   const newData = req.body;
   const sql = `update movie_data set title = $1 , release_date = $2, poster_path = $3, overview = $4 where id = $5 returning *`;
-  const updatedValue = [newData.title, newData.release_date, newData.poster_path, newData.overview, id];
-  client.query(sql, updatedValue).then(data =>
-    res.status(202).json(data.rows)
-     )
+  const updatedValue = [
+    newData.title,
+    newData.release_date,
+    newData.poster_path,
+    newData.overview,
+    id,
+  ];
+  client
+    .query(sql, updatedValue)
+    .then((data) => res.status(202).json(data.rows));
 }
 
-function deleteMovieById(req, res){
+function deleteMovieById(req, res) {
   const id = req.params.id;
   const sql = `delete from movie_data where id = ${id}`;
-  client.query(sql).then(data => 
-    res.status(204).json({
-      code: 204,
-      message: `row deleted successfully with id : ${id}`
-    })
-    ).catch(err => errorHandler(err ,req ,res));
+  client
+    .query(sql)
+    .then((data) =>
+      res.status(204).json({
+        code: 204,
+        message: `row deleted successfully with id : ${id}`,
+      })
+    )
+    .catch((err) => errorHandler(err, req, res));
 }
 
 function errorHandler(err, req, res) {
@@ -114,7 +142,9 @@ function Movie(id, title, release_date, poster_path, overview) {
 
 Movie.allData = [];
 
-
-client.connect().then(() => {
-  app.listen(PORT, () => console.log(`listening to port ${PORT}`));
-}).catch(err => console.log(err));
+client
+  .connect()
+  .then(() => {
+    app.listen(PORT, () => console.log(`listening to port ${PORT}`));
+  })
+  .catch((err) => console.log(err));
